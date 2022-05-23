8e3e10127c7a0f98ef819fc995f9405623da73d2
cloer <chltjrrb97@gmail.com>
2022-05-23 15:17:46 -0400

    ADD: main.ts

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
-async function main() {}
+import { getEpisodeList } from './crawler/crawler';
+import { FunctionError } from './lib/error.handler';
+
+async function main() {
+  const PAGE_NUM = 9;
+
+  const episodeList = await getEpisodeList(PAGE_NUM);
+  if (episodeList instanceof FunctionError) {
+    console.log(episodeList.trace);
+    console.error(episodeList);
+  } else {
+    console.log(episodeList);
+  }
+}
 
 main();
